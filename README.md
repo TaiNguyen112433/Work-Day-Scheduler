b9422aefca1c15504f42821c1bc87b22316cbbbd
unknown <tainguyen112433@gmail.com>
2021-10-18 18:02:22 -0400

    readme links

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-This is a Work Day Scheduler. This allow you to plan out your entire day by the hour. It shows the time your are available, unavailable, and current time. You can type your description of what you need to do for the day in the description and save it, by pressing the save button. by pressing the save button it will than save the description to the local file. Once you refresh the page the description will disappear
+## Application Description
+This is a Work Day Scheduler. This allow you to plan out your entire day by the hour. The hours are set 9AM to 5PM. It shows the time your are available, unavailable, and current time. You can type your description of what you need to do for the day in the description and save it, by pressing the save icon. By pressing the save icon it will than save the description to the local file and be stored. refreshing the page the description will not disappear
+
+## Screenshots
+![screenshot](/asset/image/Capture-gif.gif)
 
 
-Link to website: https://tainguyen112433.github.io/Work-Day-Scheduler/
-Deployed site: https://tainguyen112433.github.io/Work-Day-Scheduler/
-
-![Capture](https://user-images.githubusercontent.com/87043085/135685220-065bd929-3f81-422c-8db1-dc8086690f48.PNG)
-
+## Links
+- Github: https://github.com/TaiNguyen112433/Work-Day-Scheduler
+- Deyployed Website: https://tainguyen112433.github.io/Work-Day-Scheduler/
--- asset/script.js
+++ asset/script.js
@@ -2,10 +2,8 @@ let timeEl = moment().format("LLLL");
 $("#currentDay").text(timeEl)
 
 const textArea = document.querySelector(".description") 
-let saveButtons = document.querySelectorAll(".saveBtn")
-let data = localStorage.getItem(textArea)
-
-let eightAm =  document.querySelector("#am-8 .description")
+let saveButton = document.querySelectorAll(".saveBtn")
+let data = localStorage.getItem("textArea")
 let nineAm = document.querySelector("#am-9 .description")
 let tenAm = document.querySelector("#am-10 .description")
 let elevenAm =  document.querySelector("#am-11 .description")
@@ -15,24 +13,46 @@ let twoPm =  document.querySelector("#pm-2 .description")
 let threePm =  document.querySelector("#pm-3 .description")
 let fourPm =  document.querySelector("#pm-4 .description")
 let fivePm =  document.querySelector("#pm-5 .description")
-let sixPm  =  document.querySelector("#pm-6 .description")
 
-// console.log(eightAm)
 
-saveButtons.forEach(function (saveButton) {
+saveButton.forEach(function (saveButton) {
     saveButton.addEventListener("click", function(){
-        localStorage.setItem(this.parentNode.id, this.previousElementSibling.value);
-        // console.log(this.previousElementSibling.value);
-        // console.log(this.parentNode.id)
+        localStorage.setItem(saveButton.parentElement.getAttribute("id"),saveButton.previousElementSibling.value);
+        console.log(saveButton.previousElementSibling.value);
+        console.log(saveButton.parentElement.getAttribute("id"))
     })
 })
 
+const textAreas = document.querySelectorAll("textarea")
+console.log(textAreas)
+    textAreas.forEach(function(text) {
+        text.value
+    })
 
+function showvalue(){
+    let storedText = localStorage.getItem("textAreas");
 
+    if (storedText != null) {
+        document.getElementById("textAreas").value = storedText;
+    } else
+        document.getElementById("textAreas").value = 0;
+}
 
+$(".saveBtn").on("click",function(){
+    var time = $(this).parent().attr("id")
+    var input = $(this).siblings(".description").val()
+    localStorage.setItem(time, input)
+})
 
-
-
+$("#am-9 .description").val(localStorage.getItem("am-9"))
+$("#am-10 .description").val(localStorage.getItem("am-10"))
+$("#am-11 .description").val(localStorage.getItem("am-11"))
+$("#pm-12 .description").val(localStorage.getItem("pm-12"))
+$("#pm-1 .description").val(localStorage.getItem("pm-1"))
+$("#pm-2 .description").val(localStorage.getItem("pm-2"))
+$("#pm-3 .description").val(localStorage.getItem("pm-3"))
+$("#pm-4 .description").val(localStorage.getItem("pm-4"))
+$("#pm-5 .description").val(localStorage.getItem("pm-5"))
 
 
 
